6dd313f4db832313f61d06fdad1b572cbcff89f0
MortenStaunstrup <mort490fgromling@gmail.com>
2026-05-11 11:54:53 +0200

    Added CI/CD pipeline github actions file

---
+++ MembershipAPI/Models/MemberSubscription.cs
@@ -0,0 +1,16 @@
+﻿namespace MembershipAPI.Models;
+
+public class MemberSubscription
+{
+    public int MemberSubscriptionId { get; set; }
+    public int MemberId { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+    public int SubscriptionId { get; set; }
+}
+
+public enum PaymentStatus
+{
+    Cancelled,
+    Payed,
+    NotPayed
+}
---
+++ MembershipAPI/Models/Subscription.cs
@@ -0,0 +1,8 @@
+﻿namespace MembershipAPI.Models;
+
+public class Subscription
+{
+    public int SubscriptionId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
